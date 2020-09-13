be4bd994473c4da2fd8af0a96f3d3a5c2350030b
Darío LM <hello@dariolm.com>
2020-09-13 15:42:54 +0200

    Docs added to Storybook

--- src/Components/Link/Link.stories.tsx
+++ src/Components/Link/Link.stories.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Link from './Link';
 import { LINK_TARGETS } from "./Link.types";
+
 export default {
-  title: "Link"
+  title: "Link",
+  component: "Link",
+  argTypes: {
+    theme: {
+      control: {
+        type: 'select',
+        options: ['light', 'dark'],
+      },
+    },
+    target: {
+      control: {
+        type: 'select',
+        options: LINK_TARGETS,
+      },
+    },
+  },
 };
 
 const commonProps = {
@@ -11,6 +27,10 @@ const commonProps = {
   content: "I will open Empathy.co in a new tab",
 };
 
-export const Light = () => <Link {...commonProps} />;
+const Template = (args) => <Link {...args} />;
 
-export const Dark = () => <Link theme="dark" {...commonProps} />;
+export const Light = Template.bind({});
+Light.args = {theme: "light", ...commonProps};
+
+export const Dark = Template.bind({});
+Dark.args = {theme: "dark", ...commonProps};
